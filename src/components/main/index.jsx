6e5b84f916ff881232cd38e8fd6352b6e305117d
Your Name <you@example.com>
2024-09-25 12:23:49 +0200

    Main Added

--- src/components/main/index.jsx
+++ src/components/main/index.jsx
@@ -4,9 +4,15 @@ import Banner from "../../assets/img/Banner.png";
 
 export default function Main() {
   return (
-    <main>
+    <main className={styles.main}>
       <div className={styles.bannerImg}>
         <img src={Banner} className={styles.banner} />
+      </div>
+      <div className={styles.productsWrapper}>
+        <div className={styles.headingWrapper}>
+          <h1>Товары</h1>
+        </div>
+        <div className={styles.productsFeed}></div>
       </div>
     </main>
   );
